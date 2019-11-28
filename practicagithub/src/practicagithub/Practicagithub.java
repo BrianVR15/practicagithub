889192a3f49865d78f7cd06595bb2d116216b8cc
Brian Varela <brianvr15@gmail.com>
2019-11-28 16:39:25 +0100

    Fes un primer 'commit' i 'push' per actualitzar el projecte al repositori

--- practicagithub/src/practicagithub/Practicagithub.java
+++ practicagithub/src/practicagithub/Practicagithub.java
@@ -15,7 +15,21 @@ public class Practicagithub {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("He comprao leche xd");
+        System.out.println(suma());
+        System.out.println(multiplicacio());
+    }
+    
+    
+    public static int suma(){
+        int a= 5;
+        int b = 7;
+        return a+b;
+    }
+    
+    public static int multiplicacio(){
+        int a= 3;
+        int b = 2;
+        return a*b;
     }
     
 }
